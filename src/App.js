52c35a3bfdd9d48c7e4e79a319d1a825109e2e38
Jonatan Kłosko <jonatanklosko@gmail.com>
2018-03-07 22:01:00 +0100

    Add React Router.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Reboot from 'material-ui/Reboot';
 
 import Navigation from './Navigation';
+import Home from './Home';
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <Reboot />
-        <Navigation />
-      </div>
+      <Router>
+        <div>
+          <Reboot />
+          <Navigation />
+
+          <Route exact path="/" component={Home}/>
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/Home.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class Navigation extends Component {
+  render() {
+    return (
+      <div>Home</div>
+    );
+  }
+}

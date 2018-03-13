f591931068a3628c00e6c3a2fee43b0d53927d29
Jonatan Kłosko <jonatanklosko@gmail.com>
2018-03-13 17:50:12 +0100

    Switch to HashRouter in order to work with GitHub Pages. Update favicon.

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import Reboot from 'material-ui/Reboot';
 import Grid from 'material-ui/Grid';
 import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';

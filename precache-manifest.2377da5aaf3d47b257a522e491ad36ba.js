a104730179b6bd1ab4e4c0af0d077b2542055a46
Jonatan Kłosko <jonatanklosko@gmail.com>
2020-03-29 15:14:16 +0200

    Updates

---
+++ precache-manifest.2377da5aaf3d47b257a522e491ad36ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1394ebf0d9d934b33a2a623c6fdf9092",
+    "url": "/rankings/index.html"
+  },
+  {
+    "revision": "42d803a5546d2443f917",
+    "url": "/rankings/static/css/main.d0b5d989.chunk.css"
+  },
+  {
+    "revision": "efd74e894ed3a28098b4",
+    "url": "/rankings/static/js/2.bffe76e9.chunk.js"
+  },
+  {
+    "revision": "4eb8883603636345ae59314ef4e3bde6",
+    "url": "/rankings/static/js/2.bffe76e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42d803a5546d2443f917",
+    "url": "/rankings/static/js/main.d215c3d2.chunk.js"
+  },
+  {
+    "revision": "6f61b3c2304842258655",
+    "url": "/rankings/static/js/runtime-main.fc9cc485.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/rankings/precache-manifest.15a84854315d19083df19d5602a6a3ed.js"
+  "/rankings/precache-manifest.2377da5aaf3d47b257a522e491ad36ba.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/rankings/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rankings/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
